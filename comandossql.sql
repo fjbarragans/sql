bdd4cb2a6424657fa946a2ee85b355d93846fd02
fjbarragans <franciscojbarragans@gmail.com>
2023-02-03 14:49:38 -0600

    comandos

---
+++ comandossql.sql
@@ -0,0 +1,5 @@
+ssh -i "ssh-login.pem" ubuntu@52.53.156.173
+
+CREATE TABLE datospaquito (columna0 varchar(10), columna1 varchar(10), columna2 int)
+
+insert into datospaquito (columna0, columna1, columna2) values ('paquito0', 'paquito1', 2)
